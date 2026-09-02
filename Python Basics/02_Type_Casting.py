42a1011ac56f1a0867fe5ee0376eae8cd336ab10
Zidaan Shaikh <szidaan830@gmail.com>
2026-09-03 02:11:54 +0530

    Add files via upload

---
+++ Python Basics/02_Type_Casting.py
@@ -0,0 +1,22 @@
+
+# TypeCasting - TypeCasting is the process of converting a Variable from one DataType into another 
+
+name = "Zidaan Shaikh"
+age = 18
+gpa = 3.6
+is_student = True
+
+# float()
+# age = print(float(age))
+
+# int()
+# gpa = int(gpa)
+# print(gpa)
+
+# str()
+age = str(age)
+age = age + "1"
+print(age)
+
+# bool()
+name = print(bool(name))
