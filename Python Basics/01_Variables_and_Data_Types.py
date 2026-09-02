ace2adb67d2d01ad9a141e1f3bd60e688aef08f4
Zidaan Shaikh <szidaan830@gmail.com>
2026-09-03 02:39:46 +0530

    Update 01_Variables_and_Data_Types.py

--- Python Basics/01_Variables_and_Data_Types.py
+++ Python Basics/01_Variables_and_Data_Types.py
@@ -1,1 +1,45 @@
 
+# Variable = it acts as a container
+# it holds a value; values like String, integer, float, boolean.
+name = "zidaan"
+age = 18
+
+
+# strings - a series of characters; characters within quotes
+name = "zidaan"
+food = "shawarma"
+
+print(f"hello {name}")
+print(f"you like {food}")
+
+
+# integers - is a number
+age = 18
+weather = 28
+
+print(f"you are {age} years old")
+print(f"todays weather is {weather} celsius")
+
+
+# float - is a number with decimal
+gpa = 3.6
+distance = 6.9
+
+print(f"your gpa is: {gpa}")
+print(f"you ran {distance} km")
+
+
+# boolean - true or false
+is_student = True
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
+
+
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
