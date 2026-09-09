09864ffabe2f952cab05ddf96725112a06471135
Zidaan Shaikh <szidaan830@gmail.com>
2026-09-09 23:56:21 +0530

    Add files via upload

---
+++ Python Basics/07_For_Loops.py
@@ -0,0 +1,30 @@
+# for loops - Execute a block of code a fixed number of times
+    # you can iterate over a range, string, sequence, etc
+
+# Exercise 1
+for x in range(1, 11):
+    print(x)
+
+
+
+# Exercise 2
+for x in reversed(range(1, 11)):
+    print(x)
+
+print("Happy New Year!")
+
+
+
+# Exercise 3
+for x in range(1, 11, 2):
+    print(x)
+
+
+
+# Exercise 4
+# iterate over string
+credit_card = "1234-5678-9012-3456"
+
+for x in credit_card:
+    print(x)
+    
