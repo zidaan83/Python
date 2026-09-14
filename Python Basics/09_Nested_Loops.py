320a0f790900aa86264e9ef1b8f40b57fd31b31d
Zidaan Shaikh <szidaan830@gmail.com>
2026-09-15 01:17:04 +0530

    Add files via upload

---
+++ Python Basics/09_Nested_Loops.py
@@ -0,0 +1,12 @@
+# Nested loops - a loop within a loop (outer, inner)
+#                outer loop:
+#                inner loop:
+
+rows = int(input("Enter the number of rows: "))
+columns = int(input("Enter the number of columns: "))
+symbol = input("Enter a symbol to use: ")
+
+for x in range(rows):
+    for y in range(columns):
+        print(symbol, end="")
+    print()
